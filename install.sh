5821685415435e92d7a1229d9f25ca557dd6cfbc
ignotochi <ignotochi@gmail.com>
2024-01-29 15:55:53 +0100

    WIP

--- install.sh
+++ install.sh
@@ -82,21 +82,21 @@ print_color "green" "Docker containers started for merossApi."
 ########
 ########
 
-# Funzione per compilare il progetto Angular
+
 build_angular_project() {
-  repo_name="merossJS"  # Il nome del repository è fisso
+  repo_name="merossJS"  
   base_url="$1"
 
   print_color "yellow" "Building Angular project for $repo_name with base_url: $base_url..."
   
   cd "$destination_folder/$repo_name"
   
-  # Installa npm se non è già installato
+
   if ! command -v npm >/dev/null; then
-    # Chiedi all'utente se vuole installare npm con yum
+
     read -p "npm is not installed. Do you want to install it? (y/n): " install_npm
     if [ "$install_npm" = "y" ]; then
-      # Installa npm con yum (assumendo un sistema basato su yum)
+
       sudo yum install npm
     else
       print_color "red" "Error: npm is required but not installed. Exiting."
@@ -104,31 +104,17 @@ build_angular_project() {
     fi
   fi
   
-  # Continua con l'installazione delle dipendenze e la build del progetto Angular
   npm install
-  ng build --prod --base-href "/$base_url/"
+  ng build --configuration production --base-href "/$base_url/" --deploy-url "/$base_url/"
   
   print_color "green" "Angular project for $repo_name built successfully."
 }
 
-# Build del progetto Angular con il base_url fornito dall'utente
 read -p "Enter the base_url for Angular project (e.g., myapp): " user_base_url
 
-# Build del progetto Angular con i valori forniti dall'utente
 build_angular_project "$user_base_url"
 
-# Aggiungi un ritardo
-sleep 1
 
-# Cambia la directory al progetto merossJS e avvia il container Docker con httpd
-cd "$destination_folder/merossJS"
-
-# Avvia il container Docker con l'immagine di httpd
-docker-compose up -d
-
-# Aggiungi un ritardo
-sleep 1
-
-print_color "green" "Docker container started for merossJS with httpd image."
+print_color "green" "merossJS compiled with success"
 
 
